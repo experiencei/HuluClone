f51cc0d973ade3811f3bfa2415e276c8d496c8f7
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 21:38:02 +0100

    using tmdb api to build hulu

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -1,9 +1,13 @@
-
+import request from "../../utilis/request"
 
 function Nav() {
     return (
-        <nav>
-            
+        <nav> 
+           <div className="nav">
+             {Object.entries(request).map(([key , {title , url}]) => (
+                 <h2 key={key} className="cursor-pointer transition duration-100 transform hover:scale-125">{title}</h2>
+             ))}
+             </div>
         </nav>
     )
 }
