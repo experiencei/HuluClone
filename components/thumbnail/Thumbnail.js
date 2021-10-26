1970a42386328471b80b47321ead1c77ba0204f5
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 23:13:04 +0100

    working on the tmdb api wth results for Results component

--- components/thumbnail/Thumbnail.js
+++ components/thumbnail/Thumbnail.js
@@ -1,7 +1,18 @@
-function Thumbnail() {
+import Image from "next/image";
+
+function Thumbnail({result}) {
+   const BASE_URL = "https://image.tmdb.org/t/p/original/"
+
     return (
         <div>
-            
+            <Image
+              layout="responsive"
+              src={
+                  `${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`
+              }
+              height={1000}
+              width={1920}
+            />
         </div>
     )
 }
