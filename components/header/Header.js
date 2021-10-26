2b032eeb878b5e1c087b7a17447b3f1ef7d7465e
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 20:38:41 +0100

    using tmdb api to build hulu

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,6 +3,7 @@ function Header() {
     return (
         <header className="">
             <Image
+                 className="object-contain"
                 src="https://links.papareact.com/ua6"
                 height={100}
                 width={200}
