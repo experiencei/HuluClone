aa6b6a2de308c116e12cb247e0b0fce8d2f197d4
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 23:28:49 +0100

    working on the thumbnail of the component

--- components/thumbnail/Thumbnail.js
+++ components/thumbnail/Thumbnail.js
@@ -5,7 +5,7 @@ function Thumbnail({result}) {
    const BASE_URL = "https://image.tmdb.org/t/p/original/"
 
     return (
-        <div className="group cursor-pointer p-2 transition duration-200 ease-in transform">
+        <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
               layout="responsive"
               src={
