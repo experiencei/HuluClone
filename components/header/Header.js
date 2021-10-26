fe69f55c26ddc8727729af4ca8df4876f4a6a2fd
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 21:10:11 +0100

    using tmdb api to build hulu

--- components/header/Header.js
+++ components/header/Header.js
@@ -5,7 +5,7 @@ import {
 } from "@heroicons/react/outline"
 function Header() {
     return (
-        <header className="flex flex-col sm:flex-row m-5">
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
         <div className="flex flex-grow justify-evenly max-w-2xl ">
             <HeaderItem title="HOME" Icon={HomeIcon}/>
             <HeaderItem title="TRENDING" Icon={LightningBoltIcon}/>
