04f0366f959a028ea184663df1c1d2beae0d7221
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 23:04:48 +0100

    working on the tmdb api wth results for Results component

--- components/results/Results.js
+++ components/results/Results.js
@@ -1,7 +1,12 @@
+import Thumbnail from "../thumbnail/Thumbnail"
+
 function Results({ results }) {
     return (
         <div>
-            
+            {results.map ( result =>( <Thumbnail
+                   key={result.id} 
+                   result={result}
+            />))}
         </div>
     )
 }
