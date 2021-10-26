e2528bf19324edc02a00343a5a5471076d2ac030
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 20:59:14 +0100

    using tmdb api to build hulu

--- components/headerItem/HeaderItem.js
+++ components/headerItem/HeaderItem.js
@@ -1,6 +1,6 @@
 function HeaderItem({ title , Icon}) {
     return (
-        <div className="flex flex-col items-center cursor-pointer group">
+        <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
             <Icon className="h-8 mb-1"/>
             <p className="opacity-100 group-hover:opacity-100 tracking-widest"> {title} </p>
         </div>
